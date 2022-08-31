1eb3f540b1cdc76508ee1f7edf04c4bfa233af66
nickolay.grin <grinmail84@gmail.com>
2022-08-31 16:30:51 +0300

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -91,6 +91,47 @@ if ( $(this).scrollTop() > 0 && $menu.hasClass("default") ){
 	$(".input-phone").mask("+7 (999) 999-99-99");
 
 
+jQuery('.quantity').each(function() {
+		var spinner = jQuery(this),
+		input = spinner.find('input[type="number"]'),
+		btnUp = spinner.find('.quantity-up'),
+		btnDown = spinner.find('.quantity-down'),
+		min = input.attr('min'),
+		max = input.attr('max');
+
+		btnUp.click(function() {
+			var oldValue = parseFloat(input.val());
+			if (oldValue >= max) {
+				var newVal = oldValue;
+			} else {
+				var newVal = oldValue + 1;
+			}
+			spinner.find("input").val(newVal);
+			spinner.find("input").trigger("change");
+		});
+
+		btnDown.click(function() {
+			var oldValue = parseFloat(input.val());
+			if (oldValue <= min) {
+				var newVal = oldValue;
+			} else {
+				var newVal = oldValue - 1;
+			}
+			spinner.find("input").val(newVal);
+			spinner.find("input").trigger("change");
+		});
+
+	});
+
+$(".remove-basket").click(function() {
+		$(this).parent().parent().remove();
+	});
+
+$(".item-order__arrow").click(function() {
+		$(this).parent().parent().toggleClass("active");
+		$(this).parent().siblings(".item-order__content").slideToggle(200);
+	});
+
 	 // стайлер для select
 	 $('select').styler();
 
